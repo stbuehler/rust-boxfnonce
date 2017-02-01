4d6c40cade4f9396b6d984e60c61a05cb06a9502
Stefan Bühler <stbuehler@web.de>
2017-02-01 20:26:05 +0100

    add SendBoxFnOnce wrapper for Send+FnOnce
    - refactor macro to handle zero arguments, other wrapper names and
      additional function trait requirements
    - simplify test case arguments to i32 and results to simple String
    - use a non-Send type in the closure for the BoxFnOnce tests to make
      sure no accidental Send requirement comes up

---
+++ src/macros.rs
@@ -0,0 +1,31 @@
+macro_rules! build_n_args {
+	( $name:ident [$($add:tt)*]: $($var:ident: $typevar:ident),* ) => (
+		impl< $($typevar,)* Result> $name<($($typevar,)*), Result> {
+			/**
+			 * call inner function, consumes the box
+			 */
+			pub fn call(mut self $(, $var: $typevar)*) -> Result {
+				(*self.func)(($($var ,)*))
+			}
+		}
+
+		impl< $($typevar,)* Result, F: 'static + FnOnce($($typevar),*) -> Result $($add)*> From<F> for $name<($($typevar,)*), Result> {
+			fn from(func: F) -> Self {
+				let mut func = Some(func);
+				$name{
+					func: Box::new(move |($($var ,)*)| -> Result {
+						// the outer box gets consumed on a call,
+						// so the unwrap() here must always succeed
+
+						// the unwrap() needs to be done in a place
+						// where the concrete "F" type is known (due to
+						// size issues); it can't be done after type
+						// elimination (that is why Box<FnOnce> doesn't
+						// work yet)
+						func.take().unwrap()($($var),*)
+					})
+				}
+			}
+		}
+	)
+}
